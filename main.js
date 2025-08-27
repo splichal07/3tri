705793af773b6ec754a6d58419d5a138d24c6958
splichal07 <159157499+splichal07@users.noreply.github.com>
2025-08-27 11:43:37 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
-
-frm.addEventListener("submit", (e) => {
-    const nome = frm.inNome.value
-    resp.innerText = `Olá, ${nome}`
-    e.preventDefault()
-})
